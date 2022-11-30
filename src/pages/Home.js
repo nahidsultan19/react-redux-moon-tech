33877bf62d6160bf872a8f848efeee1f375c900c
nahid <sultannahid38@gmail.com>
2022-11-30 16:58:25 +0800

    added redux reducer

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
+
+const Home = () => {
+    const [products, setProducts] = useState([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+            {
+                products.map(product => <ProductCard key={product._id} product={product} s />)
+            }
+        </div>
+    );
+};
+
+export default Home;
