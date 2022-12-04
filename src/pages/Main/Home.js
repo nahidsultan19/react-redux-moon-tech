4e3f0d1609c87412fd0846107441066524d709f2
nahid <sultannahid38@gmail.com>
2022-12-04 16:45:05 +0800

    implement filter functionality

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ProductCard from '../../components/ProductCard';
-import { toggleBrand } from '../../redux/actionCreators/filterActions';
+import { toggleBrand, toggleStock } from '../../redux/actionCreators/filterActions';
 
 const Home = () => {
     const [products, setProducts] = useState([]);
+
+    const filters = useSelector(state => state.filter.filters);
+    const { brands, stock } = filters;
 
     const dispatch = useDispatch();
 
@@ -16,15 +19,13 @@ const Home = () => {
 
     const activeClass = 'text-white bg-indigo-500 border-white'
 
-    const state = useSelector(state => state);
-    console.log(state);
 
     return (
         <div className='max-w-7xl gap-14 mx-auto my-10'>
             <div className='mb-10 flex justify-end gap-5'>
-                <button className={`border px-3 py-2 rounded-full font-semibold ${activeClass}`}>In Stock</button>
-                <button onClick={() => dispatch(toggleBrand('amd'))} className={`border px-3 py-2 rounded-full font-semibold`}>AMD</button>
-                <button onClick={() => dispatch(toggleBrand('intel'))} className={`border px-3 py-2 rounded-full font-semibold`}>Intel</button>
+                <button onClick={() => dispatch(toggleStock())} className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null}`}>In Stock</button>
+                <button onClick={() => dispatch(toggleBrand('amd'))} className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('amd') ? activeClass : null}`}>AMD</button>
+                <button onClick={() => dispatch(toggleBrand('intel'))} className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('intel') ? activeClass : null}`}>Intel</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
                 {
