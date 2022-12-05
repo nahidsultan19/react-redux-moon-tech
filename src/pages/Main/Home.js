23cb19357658be70cf84eae3a5143a193d9bc88a
nahid <sultannahid38@gmail.com>
2022-12-05 17:11:18 +0800

    added curry function

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -19,6 +19,28 @@ const Home = () => {
 
     const activeClass = 'text-white bg-indigo-500 border-white'
 
+    let content;
+
+    if (products.length) {
+        content = products.map(product => <ProductCard key={product._id} product={product} s />)
+    }
+
+    if (products.length && (stock || brands.length)) {
+        content = products.filter(product => {
+            if (stock) {
+                return product.status === true;
+            }
+            return product;
+        })
+            .filter(product => {
+                if (brands.length) {
+                    return brands.includes(product.brand);
+                }
+                return product;
+            })
+            .map(product => <ProductCard key={product._id} product={product} s />)
+    }
+
 
     return (
         <div className='max-w-7xl gap-14 mx-auto my-10'>
@@ -28,9 +50,7 @@ const Home = () => {
                 <button onClick={() => dispatch(toggleBrand('intel'))} className={`border px-3 py-2 rounded-full font-semibold ${brands.includes('intel') ? activeClass : null}`}>Intel</button>
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-                {
-                    products.map(product => <ProductCard key={product._id} product={product} s />)
-                }
+                {content}
             </div>
         </div>
     );
