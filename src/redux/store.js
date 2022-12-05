b726dcb86ce89453dcc929f3dacf370c386f64a9
nahid <sultannahid38@gmail.com>
2022-12-05 21:30:45 +0800

    fixed cart items position

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,8 +3,9 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducers/rootReducer';
 import logger from 'redux-logger';
 import cartCounter from './middlewares/cartCounter';
+import thunk from 'redux-thunk';
 
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(cartCounter)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(cartCounter, thunk, logger)));
 
 export default store;
